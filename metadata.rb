ad8428277f0acd0e807f6136ed307f0841b6bae8
Darrin Eden <darrin.eden@gmail.com>
2013-02-16 07:57:09 -0500

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-name             "dnsimple"
-maintainer       "DNSimple"
-maintainer_email "ops@dnsimple.com"
-license          "Apache 2.0"
-description      "Installs/Configures dnsimple"
+name             'dnsimple'
+maintainer       'DNSimple'
+maintainer_email 'ops@dnsimple.com'
+license          'Apache 2.0'
+description      'Installs/Configures dnsimple'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          '0.5.0'
 
-recipe   "dnsimple", "Installs fog gem to use w/ the dnsimple_record"
-supports "ubuntu"
+recipe   'dnsimple', 'Installs fog gem to use w/ the dnsimple_record'
+
+supports 'ubuntu'
