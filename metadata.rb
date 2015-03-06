bb1e45d55bfa3e265c33e28e8829715f3dd35949
Aaron Kalin <akalin@martinisoftware.com>
2015-03-06 15:14:07 -0600

    Update version for official release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@dnsimple.com'
 license          'Apache 2.0'
 description      'Provides Chef LWRP for automating DNS configuration with DNSimple'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.1.0'
 
 recipe   'dnsimple', 'Installs fog gem to use w/ the dnsimple_record'
 
