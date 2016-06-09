3ee594359365459ae94c828993634dbd3ec15839
David Aronsohn <david.aronsohn@dnsimple.com>
2016-06-08 20:24:35 -0700

    Update kitchen and tests and get default passing on all chef 12

--- Berksfile
+++ Berksfile
@@ -3,4 +3,3 @@ source "https://supermarket.getchef.com"
 metadata
 
 cookbook "dnsimple_test", path: "test/fixtures/cookbooks/dnsimple_test"
-cookbook "apt"
--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "ops@dnsimple.com"
 license          "Apache 2.0"
 description      "Provides Chef LWRP for automating DNS configuration with DNSimple"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.1.0"
 issues_url       "https://github.com/dnsimple/chef-dnsimple/issues"
 source_url       "https://github.com/dnsimple/chef-dnsimple"
+version          "1.2.0"
 
 recipe   "dnsimple", "Installs fog gem to use w/ the dnsimple_record"
 
@@ -19,3 +19,4 @@ supports "rhel"
 supports "ubuntu"
 
 depends "build-essential", ">= 2.0.0"
+depends "apt"
