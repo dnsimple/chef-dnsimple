dfd215ee4b87080f459f40c6d9ea3a40792577db
Aaron Kalin <akalin@martinisoftware.com>
2016-06-09 00:35:28 -0500

    This spec is actually no longer needed

--- spec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'spec_helper'
-
-describe 'dnsimple::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
-
-  it "installs fog chef_gem" do
-    expect(chef_run).to install_chef_gem "dnsimple"
-  end
-end
