716c78c2b2e839b8acd3926ac422d76da90b7ff8
Aaron Kalin <akalin@martinisoftware.com>
2017-01-10 17:29:56 -0500

    Cleanup and update the test cookbook

--- test/cookbooks/dnsimple_test/metadata.rb
+++ test/cookbooks/dnsimple_test/metadata.rb
@@ -1,8 +1,8 @@
 name             'dnsimple_test'
-maintainer       'DNSimple'
+maintainer       'Aetrion, LLC. dba DNSimple'
 maintainer_email 'ops@dnsimple.com'
 license          'Apache 2.0'
-description      'Tests dnsimple'
-version          '0.5.0'
+description      'Tests dnsimple cookbook integration'
+version          '0.1.0'
 
 depends 'dnsimple'
