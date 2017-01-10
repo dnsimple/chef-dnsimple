b68ee6681a8accbe876f4b20c0b3c46600595662
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-01-10 19:09:22 +0100

    First spec for the simplest test recipe

---
+++ spec/unit/recipes/create_record_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'dnsimple_test::create_record' do
+  let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04', step_into: ['dnsimple_record']) }
+  let(:chef_run) { runner.converge(described_recipe) }
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+
+  it 'creates a record called "name" type A' do
+    expect(:chef_run).to create_dnsimple_record('name')
+      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', token: 'ABC')
+  end
+end
