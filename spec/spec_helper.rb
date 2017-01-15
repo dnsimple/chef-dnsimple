f77e0da991874ed46f38a551e27524c2223e6a8b
Aaron Kalin <akalin@martinisoftware.com>
2017-01-15 15:54:53 -0600

    Rip out webmock entirely
    I don't believe the edges of our library include the HTTP side of
    things. I think if we're going to control for anything it should be at
    the dnsimple api gem. How it fetches and grabs information isn't what we
    as a consumer care about. We care about how it returns data to us and
    that is it's contract with us.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,22 +5,10 @@ require 'chefspec/berkshelf'
 # Include all our libraries
 Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
 
-SPEC_ROOT = File.expand_path('../', __FILE__) unless defined?(SPEC_ROOT)
-
-Dir[File.join(SPEC_ROOT, 'support/**/*.rb')].each { |f| require f }
-
 RSpec.configure do |config|
   # Specify the operating platform to mock Ohai data from
   config.platform = 'ubuntu'
 
   # Specify the operating version to mock Ohai data from
   config.version = '14.04'
-end
-
-def http_fixture(*names)
-  File.join(SPEC_ROOT, "fixtures.http", *names)
-end
-
-def read_http_fixture(*names)
-  File.read(http_fixture(*names))
 end
