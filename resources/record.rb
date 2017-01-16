4c374cbe9ec2d274977f0e6f21072c079da9ab4b
Aaron Kalin <akalin@martinisoftware.com>
2017-01-16 00:15:30 -0600

    Cleanup copyright header

--- resources/record.rb
+++ resources/record.rb
@@ -1,13 +1,14 @@
 #
-# Copyright:: Copyright (c) 2010-2011, Heavy Water Software
-# Copyright:: Copyright (c) 2011, Joshua Timberman
-# License:: Apache License, Version 2.0
+# Cookbook Name:: dnsimple
+# Resource:: record
+#
+# Copyright 2014-2017 Aetrion, LLC dba DNSimple
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
