f1cd1cbdd30fac164d6e5d5e9d8156ee571a09d4
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-01-17 12:38:33 +0100

    This are the first tests that should pass

--- spec/recipes/create_record_spec.rb
+++ spec/recipes/create_record_spec.rb
@@ -4,12 +4,14 @@ describe 'dnsimple_test::create_record' do
   let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04', step_into: ['dnsimple_record']) }
   let(:chef_run) { runner.converge(described_recipe) }
 
-  xit 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
+  let(:dnsimple_record) { instance_double('dnsimple_record', name: name, content: '1.1.1.1', domain: 'example.com', access_token: 'ABC') }
+
+  it 'converges successfully' do
+    expect { chef_run }.to_not raise_error
   end
 
-  xit 'creates a record called "name" type A' do
-    expect(:chef_run).to create_dnsimple_record('name')
-      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', token: 'ABC')
+  it 'creates a record called "name" type A' do
+    expect { chef_run }.to create_dnsimple_record('name')
+      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', access_token: 'ABC')
   end
 end
