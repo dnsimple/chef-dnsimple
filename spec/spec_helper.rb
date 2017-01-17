a2c16491aa2e6eb00e75c1028dee6065286c40af
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-01-17 12:35:44 +0100

    We need matchers to be included

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # Added by ChefSpec
 require 'chefspec'
 require 'chefspec/berkshelf'
+require './libraries/matchers'
 
 RSpec.configure do |config|
   # Specify the operating platform to mock Ohai data from
