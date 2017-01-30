5f19c16a7e53e9fbcfb9ec8038f49f17517294a3
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-01-30 12:24:43 +0100

    Adding stubs for auth and record creation

--- spec/recipes/create_record_spec.rb
+++ spec/recipes/create_record_spec.rb
@@ -6,12 +6,19 @@ describe 'dnsimple_test::create_record' do
 
   let(:dnsimple_record) { instance_double('dnsimple_record', name: name, content: '1.1.1.1', domain: 'example.com', access_token: 'ABC') }
 
+  before do
+    stub_authentication
+
+    stub_request(:post, %r{/v2/1/zones/example.com/records$}).
+      to_return(read_http_fixture("createZoneRecord/created.http"))
+  end
+
   it 'converges successfully' do
     expect { chef_run }.to_not raise_error
   end
 
   it 'creates a record called "name" type A' do
-    expect { chef_run }.to create_dnsimple_record('name')
-      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', access_token: 'ABC')
+    expect(chef_run).to create_dnsimple_record('name')
+      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', access_token: 'ABC123')
   end
 end
