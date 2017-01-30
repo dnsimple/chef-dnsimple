2cda6466932b10ab1d8befaaea6bfcfa8cee3fe2
Jacobo García López de Araujo <jacobo.garcia@gmail.com>
2017-01-30 18:24:52 +0100

    Adding shared context

--- spec/recipes/create_record_spec.rb
+++ spec/recipes/create_record_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
 describe 'dnsimple_test::create_record' do
-  let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04', step_into: ['dnsimple_record']) }
-  let(:chef_run) { runner.converge(described_recipe) }
+  include_context 'dnsimple'
 
   before do
-    stub_authentication
-
     stub_request(:post, %r{/v2/1/zones/example.com/records$})
       .to_return(read_http_fixture('createZoneRecord/created.http'))
   end
 
-  it 'converges successfully' do
-    expect { chef_run }.to_not raise_error
-  end
+  context 'with a no existing record with the same name' do
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
 
-  it 'creates a record called "name" type A' do
-    expect(chef_run).to create_dnsimple_record('name')
-      .with(type: 'A', content: '1.1.1.1', domain: 'example.com', access_token: 'ABC123')
+    it 'creates a record called "name" type A' do
+      expect(chef_run).to create_dnsimple_record('name')
+        .with(type: 'A', content: '1.1.1.1', domain: 'example.com', access_token: 'ABC123')
+    end
   end
 end
