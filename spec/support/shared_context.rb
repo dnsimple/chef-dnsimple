9dc15d28617d1dc3265427cba7e7e99b01a9ea4a
Aaron Kalin <akalin@martinisoftware.com>
2017-02-07 16:52:15 -0600

    Remove web fixtures and supporting stuff

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,8 +1,4 @@
 shared_context 'dnsimple' do
   let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04', step_into: ['dnsimple_record']) }
   let(:chef_run) { runner.converge(described_recipe) }
-
-  before do
-    stub_authentication
-  end
 end
