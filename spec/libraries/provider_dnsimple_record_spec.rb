ae7c0eec695f61500d3d26c368206f778facdd69
Aaron Kalin <akalin@martinisoftware.com>
2017-02-07 23:21:16 -0600

    Setup the zones doubles too

--- spec/libraries/provider_dnsimple_record_spec.rb
+++ spec/libraries/provider_dnsimple_record_spec.rb
@@ -22,11 +22,12 @@ describe Chef::Provider::DnsimpleRecord do
       allow(@provider).to receive(:dnsimple_gem_require).and_return(true)
     end
 
-    let(:client) { double('client', identity: identity) }
+    let(:client) { double('client', identity: identity, zones: zones) }
     let(:identity) { double('identity', whoami: response) }
     let(:response) { double('response', data: data) }
     let(:data) { double('data', account: account) }
     let(:account) { double('account', id: 1) }
+    let(:zones) { double('zones') }
 
     it 'returns record object if record name matches' do
       expect(@provider.create_record.name).to eq('example_record')
