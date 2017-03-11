a0063816dcc4eae4218951375927b503652730f7
Aaron Kalin <akalin@martinisoftware.com>
2017-03-10 23:48:12 -0500

    Break this into multiple lines

--- libraries/dnsimple_resource.rb
+++ libraries/dnsimple_resource.rb
@@ -4,7 +4,9 @@ class Chef
     class DnsimpleResource < Chef::Resource
       attr_accessor :exists
 
-      def after_created; sensitive(true); end
+      def after_created
+        sensitive(true)
+      end
 
       property :access_token, kind_of: String, required: true
       property :base_url, kind_of: String, default: 'https://api.dnsimple.com'
