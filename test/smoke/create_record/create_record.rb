61a5521ab624efb89aa53b5c9f6dc0d89df46d32
Aaron Kalin <akalin@martinisoftware.com>
2017-03-11 03:06:56 -0500

    Not a very helpful error message
    Was giving me wrong number of arguments and swalling the line that
    created the error. :(

--- test/smoke/create_record/create_record.rb
+++ test/smoke/create_record/create_record.rb
@@ -1,4 +1,4 @@
-token = attribute('dnsimple_token')
+token = attribute('dnsimple_token', description: 'The dnsimple API token')
 describe dnsimple_zone('dnsimple.net', name: 'arecord', token: token, type: 'A') do
   its('value') { should eq '1.2.3.4' }
   its('ttl') { should eq '3600' }
