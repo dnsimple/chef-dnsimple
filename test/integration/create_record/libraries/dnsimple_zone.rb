2ca70552e1a0cbfec2ee4d97dc9dc739162cc7cb
Aaron Kalin <akalin@martinisoftware.com>
2017-03-15 16:13:57 -0500

    Remove the module namespacing
    I'm not entirely sure where I gleaned this example from, but it seemed
    to be part of the cause of troubles for me.

--- test/integration/create_record/libraries/dnsimple_zone.rb
+++ test/integration/create_record/libraries/dnsimple_zone.rb
@@ -1,49 +1,47 @@
 require 'dnsimple'
 
-module Inspec::Resources
-  class DnsimpleZone < Inspec.resource(1)
-    name 'dnsimple_zone'
-    desc 'Use the dnsimple_zone InSpec audit resource to verify zone data via the dnsimple API.'
-    example "
+class DnsimpleZone < Inspec.resource(1)
+  name 'dnsimple_zone'
+  desc 'Use the dnsimple_zone InSpec audit resource to verify zone data via the dnsimple API.'
+  example "
       describe dnsimple_zone('example.com', name: 'server', token: 'abc321') do
         its('value') { should eq '1.2.3.4' }
         its('ttl') { should eq '3600' }
       end
-    "
+  "
 
-    def initialize(zone, name: nil, token: nil, type: nil)
-      @zone = zone
-      @name = name
-      @token = token
-      @type = type
-    end
+  def initialize(zone, name: nil, token: nil, type: nil)
+    @zone = zone
+    @name = name
+    @token = token
+    @type = type
+  end
 
-    def value
-      response.value if response.respond_to?(:value)
-    end
+  def value
+    response.value if response.respond_to?(:value)
+  end
 
-    def ttl
-      response.ttl if response.respond_to?(:ttl)
-    end
+  def ttl
+    response.ttl if response.respond_to?(:ttl)
+  end
 
-    def to_s
-      "zone record lookup on #{@zone} for #{@name}"
-    end
+  def to_s
+    "zone record lookup on #{@zone} for #{@name}"
+  end
 
-    private
+  private
 
-    def response
-      sandbox_url = 'https://api.sandbox.dnsimple.com'
-      client = Dnsimple::Client.new(base_url: sandbox_url, access_token: @token)
+  def response
+    sandbox_url = 'https://api.sandbox.dnsimple.com'
+    client = Dnsimple::Client.new(base_url: sandbox_url, access_token: @token)
 
-      whoami = client.identity.whoami.data
-      account_id = whoami.account.id
+    whoami = client.identity.whoami.data
+    account_id = whoami.account.id
 
-      filter = {}
-      filter = { type: @type } if @type
-      client.zones.records(account_id, @zone, filter: filter).data.select do |record|
-        record.name == @name
-      end.first
-    end
+    filter = {}
+    filter = { type: @type } if @type
+    client.zones.records(account_id, @zone, filter: filter).data.select do |record|
+      record.name == @name
+    end.first
   end
 end
