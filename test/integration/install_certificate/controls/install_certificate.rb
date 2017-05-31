d2153aed161ae19cd8ca0ef1414c3a4e473591fe
Anthony Eden <me@anthonyeden.com>
2017-05-31 08:01:20 +0200

    Test cert and key with x509_certificate and key_rsa, respectively

--- test/integration/install_certificate/controls/install_certificate.rb
+++ test/integration/install_certificate/controls/install_certificate.rb
@@ -1,8 +1,10 @@
-# TODO: verify with x509_certificate
-describe file('/etc/apache2/ssl/dnsimple.xyz.crt') do
-  its('content') { should match /-----BEGIN CERTIFICATE-----/ }
+describe x509_certificate('/etc/apache2/ssl/dnsimple.xyz.crt') do
+  its('key_length') { should be 2048 }
+  its('subject.CN') { should eq 'www.dnsimple.xyz' }
 end
-# TODO: verify with key_rsa
-describe file('/etc/apache2/ssl/dnsimple.xyz.key') do
-  its('content') { should match /-----BEGIN RSA PRIVATE KEY-----/ }
+
+describe key_rsa('/etc/apache2/ssl/dnsimple.xyz.key') do
+  it { should be_private }
+  it { should be_public }
+  its('key_length') { should be 2048 }
 end
