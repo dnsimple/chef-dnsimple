313ba44dde7515a3c4f5e28941939a5a8b1a280f
David Aronsohn <WagThatTail@Me.com>
2017-10-10 15:51:14 +0100

    Testing to chef 13 (#50)
    * Testing to chef 13
    
    * Fix licencing slug & remove nonexistant platform

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'dnsimple'
 maintainer       'Aetrion, LLC. dba DNSimple'
 maintainer_email 'ops@dnsimple.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Provides Chef Resource for automating DNS configuration with DNSimple'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/dnsimple/chef-dnsimple/issues'
@@ -12,6 +12,6 @@ chef_version '>= 12.8'
 
 gem 'dnsimple', '>= 4.0'
 
-%w(amazon centos debian fedora freebsd redhat rhel ubuntu).each do |os|
+%w(amazon centos debian fedora freebsd redhat ubuntu).each do |os|
   supports os
 end
