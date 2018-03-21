ebda7a935c62a2f82562c73afe398377b168320e
Aaron Kalin <akalin@martinisoftware.com>
2018-03-21 14:05:46 -0500

    I haven't meta test I couldn't metatest :wink:

--- test/integration/update_record/controls/update_record.rb
+++ test/integration/update_record/controls/update_record.rb
@@ -1,6 +1,6 @@
 token = attribute('dnsimple_token', description: 'The dnsimple API token')
 test_domain = attribute('test_domain', description: 'The domain to test against the API')
 describe dnsimple_zone(test_domain, name: 'cnamerecord', token: token, type: 'CNAME') do
-  its('content') { should eq 'testing.dnsimple.xyz' }
+  its('content') { should eq "testing.#{test_domain}" }
   its('ttl') { should eq 60 }
 end
