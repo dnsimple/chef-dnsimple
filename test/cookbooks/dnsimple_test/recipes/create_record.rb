205d8c57c0c2de3ae813f0413422a7d3895995ad
Amelia Aronsohn <squirrel@wearing.black>
2020-02-06 17:09:48 -0800

    Add extra tests
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- test/cookbooks/dnsimple_test/recipes/create_record.rb
+++ test/cookbooks/dnsimple_test/recipes/create_record.rb
@@ -7,3 +7,13 @@ dnsimple_record 'arecord' do
   access_token node['dnsimple']['access_token']
   base_url node['dnsimple']['base_url']
 end
+
+dnsimple_record 'arecord2' do
+  record_name 'arecord'
+  type 'A'
+  content '1.2.3.4'
+  ttl 3600
+  domain node['dnsimple']['test_domain']
+  access_token node['dnsimple']['access_token']
+  base_url node['dnsimple']['base_url']
+end
