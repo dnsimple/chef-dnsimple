eed1049e39c4773a2e7fe5bd3981298ed0960186
Santiago Traversa <santiago.traversa@gmail.com>
2021-06-10 20:21:28 -0300

    Another test fix attempt

--- test/integration/create_record/controls/create_record.rb
+++ test/integration/create_record/controls/create_record.rb
@@ -1,5 +1,5 @@
-token = input('dnsimple_token')
-test_domain = input('test_domain')
+token = input('dnsimple_token', description: 'The dnsimple API token')
+test_domain = input('test_domain', description: 'The domain to test against the API')
 describe dnsimple_zone(test_domain, name: 'arecord', token: token, type: 'A') do
   its('content') { should eq '1.2.3.4' }
   its('ttl') { should eq 3600 }
--- test/integration/update_record/controls/update_record.rb
+++ test/integration/update_record/controls/update_record.rb
@@ -1,5 +1,5 @@
-token = input('dnsimple_token')
-test_domain = input('test_domain')
+token = input('dnsimple_token', description: 'The dnsimple API token')
+test_domain = input('test_domain', description: 'The domain to test against the API')
 describe dnsimple_zone(test_domain, name: 'cnamerecord', token: token, type: 'CNAME') do
   its('content') { should eq "testing.#{test_domain}" }
   its('ttl') { should eq 60 }
