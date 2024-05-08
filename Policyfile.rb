d456b666c0c480f3171e15013253573e684912ef
Santiago Traversa <santiago.traversa@gmail.com>
2024-05-08 11:40:41 -0300

    Renema/move tests

--- Policyfile.rb
+++ Policyfile.rb
@@ -5,11 +5,11 @@ name 'dnsimple'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'dnsimple_test::default'
-named_run_list :create_test_records, %w(dnsimple_test::default dnsimple_test::reset_test_environment dnsimple_test::create_record)
-named_run_list :update_test_records, %w(dnsimple_test::default dnsimple_test::reset_test_environment dnsimple_test::update_record)
+run_list 'test::default'
+named_run_list :create_test_records, %w(test::default test::reset_test_environment test::create_record)
+named_run_list :update_test_records, %w(test::default test::reset_test_environment test::update_record)
 
 # Specify a custom source for a single cookbook:
 # cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
 cookbook 'dnsimple', path: '.'
-cookbook 'dnsimple_test', path: 'test/cookbooks/dnsimple_test'
+cookbook 'test', path: 'test/cookbooks/test'
